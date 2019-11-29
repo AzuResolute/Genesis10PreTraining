41afb27ca5ed49e52af45698e05a565e510614b4
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-11-29 15:54:48 -0500

    Day 4 - Adding JavaScript

--- Restaurant3/script/script.js
+++ Restaurant3/script/script.js
@@ -1,7 +1,34 @@
-let menu = [
-    {
-        name: 'Burger and Fries',
-        catch: 'An American Classic!',
-        description: 'Your choice - with or without cheese '
+// classes
+
+class MenuItem {
+    constructor(name, eyeCatcher, description){
+        _name = name;
+        _eyeCatcher = eyeCatcher;
+        _description = description;
     }
-]
+}
+
+class Restaurant {
+    constructor(name/*menuItems*/) {
+        this._name = name;
+        /*_menuItems = menuItems;*/
+    }
+    DisplayName = () => {
+        document.getElementById("RestaurantTitle").innerText = this._name;
+    }
+
+}
+
+
+// program
+    // menu = [
+    //     {
+    //         name: 'Burger and Fries',
+    //         catch: 'An American Classic!',
+    //         description: 'Your choice - with or without cheese '
+    //     }
+    // ]
+
+let InakinKita = new Restaurant(
+    "Inakin Kita ");
+console.log(InakinKita._name);
