bf1f073dae5211a7bd082ae4c7e33e138d825e7c
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-12-06 00:05:52 -0500

    Final Checks on Restaurant3

--- Restaurant3/scripts/form.js
+++ Restaurant3/scripts/form.js
@@ -1,1 +1,29 @@
 // validate forms
+
+let IsValid = () => {
+    let {name, email, phone} = document.contact;
+    let validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let validPhone = /((\(\d{3}\) ?)|(\d{3}-)) ?\d{3}-\d{4}$/;
+    
+    if(name.value === "" ) {
+        name.focus() ;
+        alert("Invalid Name");
+        return false;
+    } else if (email.value === "" || !validEmail.test(email.value)) {
+        email.focus();
+        alert("Invalid Email");
+        return false;
+    } else if (phone.value === "" || !validPhone.test(phone.value)) {
+        email.focus();
+        alert("Invalid Phone");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+let Send = () => {
+    if(IsValid()){
+        alert("Input Validated and Received. Thanks!");
+    }
+}
